05698dca092d560a4f791cf8ce8cc27c0709e210
nickweinberg <nick@joinhoney.com>
2017-02-07 00:22:08 -0800

    idk some sqlz

--- docker-up.sh
+++ docker-up.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# idk why but  gotta run these manually lol???
+
 docker run --name smash-db -d \
   -e MYSQL_ROOT_PASSWORD=123 \
   -e MYSQL_DATABASE=smash -e MYSQL_USER=nick -e MYSQL_PASSWORD=123 \
@@ -8,7 +10,6 @@ docker run --name smash-db -d \
 
 echo "Waiting for DB to start up..."  
 docker exec smash-db mysqladmin --silent --wait=30 -unick -p123 ping
-
 
 echo "Setting up initial data..."  
 docker exec -i smash-db mysql -unick -p123 smash < setup.sql  
--- setup.sql
+++ setup.sql
@@ -2,3 +2,22 @@ create table users (user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHA
 insert into users (name) values ('nick');
 insert into users (name) values ('duane');
 
+create table matches (
+  match_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  tournament_id INT NOT NULL,
+  status VARCHAR(80) NOT NULL,
+  player_one_id INT NOT NULL,
+  player_two_id INT NOT NULL,
+  player_one_score INT,
+  player_two_score INT
+);
+
+create table tournaments (tournament_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name TEXT);
+
+create table bets (
+  bet_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  tournament_id INT NOT NULL,
+  bettor_id INT NOT NULL,
+  player_id INT NOT NULL,
+  amount INT NOT NULL
+);
