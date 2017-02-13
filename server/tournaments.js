1560c698f4c3aff3a6b706b40e6669f8c5f353fe
Duane Garber <duane@joinhoney.com>
2017-02-12 19:48:52 -0800

    Adding handlebars

--- server/tournaments.js
+++ server/tournaments.js
@@ -1,29 +1,44 @@
 const { getConnection } = require('./db');
 
 module.exports = {
-  createTournament: function(name, cb) {
-    getConnection().query(`
-        INSERT INTO tournaments
-          (name)
-        VALUES (?)
-      `,
-      [name],
-      (err = null, results) => {
-        return cb(err, (results) ? name : null)
-      }
-    )
-  },
-  getTournament: function(tournamentId, cb) {
-    getConnection().query(`
-        SELECT *
-        FROM tournaments
-        WHERE
-          tournament_id = ?
-      `,
-      [ tournamentId ],
-      (err = null, results) => {
-        return cb(err, JSON.parse(JSON.stringify(...results)));
-      }
-    );
-  }
+  createTournament,
+  getTournament,
+  getAllTournaments,
+}
+
+function createTournament (name, cb) {
+  getConnection().query(`
+      INSERT INTO tournaments
+        (name)
+      VALUES (?)
+    `,
+    [name],
+    (err = null, results) => {
+      return cb(err, (results) ? name : null)
+    }
+  )
+}
+
+function getTournament (tournamentId, cb) {
+  getConnection().query(`
+      SELECT *
+      FROM tournaments
+      WHERE
+        tournament_id = ?
+    `,
+    [ tournamentId ],
+    (err = null, results) => {
+      return cb(err, JSON.parse(JSON.stringify(...results)));
+    }
+  );
+}
+
+function getAllTournaments (cb) {
+  getConnection().query(`
+      SELECT *
+      FROM tournaments
+    `, (err = null, results) => {
+      return cb(err, JSON.parse(JSON.stringify(results)));
+    }
+  )
 }
